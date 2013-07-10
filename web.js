4325e186167713445f9010290898bb4f516c44e8
helio <helio@helio.com.br>
2013-07-10 01:15:04 -0300

    web.js reading index.html

--- web.js
+++ web.js
@@ -1,12 +1,24 @@
+/* Stanford/Coursera - StartUP Enginnering course
+*/
+
+
 var express = require('express');
+
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+    fs.readFile('index.html', function (err, data) {
+        if (err) throw err;
+        buffer = new Buffer(data);
+        response.send(buffer.toString('utf-8'));
+    });
+    
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+    app.listen(port, function() {
+    console.log("Listening on " + port);
 });
